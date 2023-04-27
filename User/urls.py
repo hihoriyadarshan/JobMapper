1949e127012cfa75ab8c57de973a740318a6c410
hihoriyadarshan <112104707+hihoriyadarshan@users.noreply.github.com>
2023-04-27 21:16:31 +0530

    change password userside

--- User/urls.py
+++ User/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('home',views.home,name='home'),
     path('jobapply1', views.jobapply1,name='jobapply1'), 
     path('jobapply', views.jobapply,name='jobapply'),           
-
+    path('user_change_pswd',views.user_change_pswd,name='user_change_pswd'),
+    path('change_password1',views.change_password1,name='change_password1'),
 ]
--- User/views.py
+++ User/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from User.forms import JobApplicationForm
 from User.models import JobApplication
+from SignUp.models import SignUp
 from Company.models import jobpost
-
+from django.contrib.auth.hashers import make_password ,check_password
+from django.contrib import messages
 from django.http import HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
+
 
 # Create your views here.
 #user profile
@@ -42,3 +46,37 @@ def jobapply(request):
 
     return render (request, "apply.html")
     # return HttpResponse('Fail')
+
+
+#chnage password user
+
+def user_change_pswd(request):
+    return render(request,'user_change_pswd.html')
+
+def change_password1(request):
+    if request.method == "POST":    
+        old_pswd = request.POST.get("password")
+        username1 = request.POST.get("username1")
+    
+        obj = get_object_or_404(SignUp, username = username1)
+        
+        result = check_password(old_pswd, obj.password)
+        # if obj.companyname == 2 and result == True:
+        if result == True:
+        
+            new_pswd = request.POST.get("new_password")
+            cnfm_pswd = request.POST.get("cnfm_password")
+            
+            if new_pswd == cnfm_pswd:
+                
+                obj.password = make_password(new_pswd)
+                obj.save()
+                
+                messages.success(request, "Password Changed successfully!")
+                return render(request,'user_change_pswd.html')
+            else :
+                messages.error(request, "New Password and Confirm Password doesn't match!")
+                return render(request, 'user_change_pswd.html')
+        else:
+            messages.error(request, "Old is password is not correct!")
+            return render(request, 'user_change_pswd.html')
